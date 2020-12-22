b32325277dc386ec67a3d11849f8ad91d6bc04ad
JustinPoon <67882809+JustinPoon@users.noreply.github.com>
2020-12-22 13:59:19 +1100

    Testing Routing

--- src/index.js
+++ src/index.js
@@ -2,6 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom";
 import HomePage from "./Pages/HomePage";
 import registerServiceWorker from "./registerServiceWorker";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-ReactDOM.render(<HomePage />, document.getElementById("root"));
+ReactDOM.render(
+  <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Work</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Switch>
+    </div>
+  </Router>,
+  document.getElementById("root")
+);
 registerServiceWorker();
+
+function About() {
+  return <h2>About</h2>;
+}
