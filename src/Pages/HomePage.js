198dfd27372edfe23896890b705105d9e17ac2de
JustinPoon <67882809+JustinPoon@users.noreply.github.com>
2020-12-28 07:39:22 +1100

    Final Commit

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -5,28 +5,36 @@ import mondaymockup from "../Assets/mondaymockup.png";
 import IntroSection from "../Components/IntroSection";
 import WorkExperience from "../Components/WorkExperience";
 import ContactSection from "../Components/ContactSection";
+import { Router, Route, Link } from "react-router-dom";
+import "../index.scss";
 
 class HomePage extends Component {
   render() {
     return (
-      <body>
+      <div>
         <IntroSection></IntroSection>
-        <WorkExperience
-          styles={{ backgroundImage: "linear-gradient(#dbcdff, #a2c7dc)" }}
-          title="Project Management"
-          category="Western Health Internship"
-          image={mondaymockup}
-        ></WorkExperience>
-        <WorkExperience
-          styles={{
-            backgroundImage: "linear-gradient(to top left, #fbd5e8, #ffeec4)",
-          }}
-          title="Sustainability Projects"
-          category="Western Health Internship"
-          image={sharps}
-        ></WorkExperience>
+        <Link to={"/ProjectManagement"} className="link-style--plain">
+          <WorkExperience
+            styles={{ backgroundImage: "linear-gradient(#dbcdff, #a2c7dc)" }}
+            title="Project Management"
+            category="Western Health Internship"
+            image={mondaymockup}
+          >
+            <div onClick={() => history.push("/ProjectManagement")}></div>
+          </WorkExperience>
+        </Link>
+        <Link to={"/Sustainability"} className="link-style--plain">
+          <WorkExperience
+            styles={{
+              backgroundImage: "linear-gradient(to top left, #fbd5e8, #ffeec4)",
+            }}
+            title="Sustainability Projects"
+            category="Western Health Internship"
+            image={sharps}
+          ></WorkExperience>
+        </Link>
         <ContactSection></ContactSection>
-      </body>
+      </div>
     );
   }
 }
